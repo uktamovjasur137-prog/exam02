0a93ab2a8fd77de85fd264fa7f95c974a57364ab
Jasur <uktamovjasur137@gmail.com>
2026-04-20 03:49:52 -0700

    done

--- task15.py
+++ task15.py
@@ -1,9 +1,11 @@
 def find_top_students(grades: dict) -> dict:
-    result = {'max_grade': 0, 'students': [] }
-    for name, grade in grades.items():
-        if grade > result['max_grade']:
-            result['max_grade'] = grade
-            result['students'] = [name]
+    result = {}
+    x = max(grades.values())
+    result['grade'] = x
+    result['students'] = []
+    for student, grade in grades.items():
+        if grade == x:
+            result['students'].append(student)     
     return result
 
 x = find_top_students({'John': 5, 'Jane': 4, 'Bob': 5, 'Alice': 3})
