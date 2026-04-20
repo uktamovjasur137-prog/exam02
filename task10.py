1c63cd24218b1c5b4bc022199ee8d8e30560e7c9
Jasur <uktamovjasur137@gmail.com>
2026-04-20 04:12:33 -0700

    done

--- task10.py
+++ task10.py
@@ -1,5 +1,5 @@
 def sort_numbers(numbers: list, reverse: bool = False) -> list:
-    result = sorted(numbers, reverse=reverse)
+    result = sorted(numbers, key=lambda x: x, reverse=reverse)
     return result
 
 x = sort_numbers([1, 5, 6, 2, 4])
